e9bd9f6284559360ce62fecb7321c6f2c5e9aa75
yigal <yigal@yigal-virtual-machine>
2015-01-23 19:15:04 +0200

    Commit after changing eclipse project settings

---
+++ my_dir.c
@@ -0,0 +1,28 @@
+/*
+ * my_rm.c - Custom implementation of ls on ext2fs
+ *
+ *   Created on: Jan 23, 2015
+ *      Student: Yigal Alexander
+ *      	 ID: 306914565
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "debugutil.h"
+
+void print_usage() {
+	printf("Usage: my_dir\n");
+}
+
+
+int main(int argc, char * argv[]) {
+
+	if (argc==2) { /* check args*/
+
+	}
+	print_usage();
+	exit (1);
+
+
+}
